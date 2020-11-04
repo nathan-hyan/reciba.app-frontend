6e3e21b20856da90c4bcbe9991f3bf77a569a446
Hy-An Exequiel <nathan.hyan2048@gmail.com>
2020-11-04 12:37:52 -0300

    Added reference to githubpages

--- src/Pages/invoice/generate/GenerateInvoice.tsx
+++ src/Pages/invoice/generate/GenerateInvoice.tsx
@@ -18,7 +18,7 @@ import { IdGeneration } from "../../../Context/IdGeneration";
 const ENDPOINT =
   process.env.NODE_ENV !== "production"
     ? "http://192.168.100.6:8000"
-    : "https://billsapp.herokuapp.com/";
+    : "https://nathan-hyan.github.io/reciba-app-frontend/";
 const socket = io.connect(ENDPOINT, { transports: ["websocket"] });
 
 export default function GenerateInvoice() {
