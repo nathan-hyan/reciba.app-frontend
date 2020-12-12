b80f854aaeb5dd2bea1298692ee56da144c6a134
Hy-An Exequiel <nathan.hyan2048@gmail.com>
2020-12-12 10:43:12 -0300

    fixed endpoints

--- src/constants/endpoint.ts
+++ src/constants/endpoint.ts
@@ -1,9 +1,9 @@
-// export const endpoints = {
-//   backend: 'https://recibapp.herokuapp.com/',
-//   frontend: 'https://reciba.app/',
-// };
-
 export const endpoints = {
-  backend: 'http://192.168.100.57:8000/',
-  frontend: 'http://192.168.100.57:3000/',
+  backend: 'https://recibapp.herokuapp.com/',
+  frontend: 'https://reciba.app/',
 };
+
+// export const endpoints = {
+//   backend: 'http://192.168.100.57:8000/',
+//   frontend: 'http://192.168.100.57:3000/',
+// };
