f43b0dbc75eb3b6c1cf1ecf1b66ba02c0aa60c58
Hy-An Exequiel <nathan.hyan2048@gmail.com>
2020-12-19 00:05:49 -0300

    Endpoint changed

--- src/constants/endpoint.ts
+++ src/constants/endpoint.ts
@@ -1,9 +1,9 @@
-// export const endpoints = {
-//   backend: 'https://recibapp.herokuapp.com/',
-//   frontend: 'https://reciba.app/',
-// };
-
 export const endpoints = {
-  backend: 'http://nathanhyan.com:8000/',
-  frontend: 'http://nathanhyan.com:3000/',
+  backend: 'https://recibapp.herokuapp.com/',
+  frontend: 'https://reciba.app/',
 };
+
+// export const endpoints = {
+//   backend: 'http://nathanhyan.com:8000/',
+//   frontend: 'http://nathanhyan.com:3000/',
+// };
