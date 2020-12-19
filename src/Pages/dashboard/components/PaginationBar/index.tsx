38ef04a86be562392d0218fd6c2b9856c1694719
Hy-An Exequiel <nathan.hyan2048@gmail.com>
2020-12-18 21:30:56 -0300

    Internationalization and changes

---
+++ src/Pages/dashboard/components/PaginationBar/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Col } from 'react-bootstrap';
+
+export default function PaginationBar() {
+  return (
+    <Col className="bg-white rounded shadow p-3 mb-3 text-center">
+      Ey locura
+    </Col>
+  );
+}
